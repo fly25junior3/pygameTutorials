45bc64adfd8a432e4420b1284f614d6185892adc
junior <fly25junior3@gmail.com>
2026-03-25 11:30:07 -0300

    fishing the first part of the tutorial. (create a black window)

--- game.py
+++ game.py
@@ -11,10 +11,25 @@ class App:
         pygame.init();
         self.display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE);
         self._running = True;
-
-    def on_execute(self):
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False;
+    def on_loop(self):
         pass;
-
+    def on_render(self):
+        pass;
+    def on_cleanup(self):
+        pygame.quit();
+    
+    def on_execute(self):
+        if self.on_init() == False:
+            self.running = False;
+        while(self.running):
+            for event in pygame.event.get():
+                self.on_event(event);
+            self.on_loop();
+            self.on_render();
+        self.on_cleanup();
 if __name__ == "__main__":
     theApp = App(); 
     theApp.on_execute();
